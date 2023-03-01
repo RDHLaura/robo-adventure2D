883973fe01e8d093dc56d783a7471e242a5f20d0
rdhlaura <laurarodriguezdeharo@outlook.es>
2023-03-01 16:20:26 +0100

    cambio en imagenes y modificaciones del diseño

--- robo-adventure2Dweb/src/assets/js/carrusel-fondos.js
+++ robo-adventure2Dweb/src/assets/js/carrusel-fondos.js
@@ -11,20 +11,29 @@ previous.addEventListener('click', ()=>{
   let translate = container.style.transform;
   if(translate == 'translateX(0%)')
     return false
-  else if (translate == 'translateX(-33.333%)')
+  else if (translate == 'translateX(-33.333%)') {
     container.style.transform = `translateX(0%)`;
+    previous.style.color= "transparent"
+  }
   else if(translate == 'translateX(-66.666%)') {
     container.style.transform = `translateX(-33.333%)`;
+    next.style.color= "#d9eee6";
+
   }
   let audio = document.getElementById("down");
   audio.play();
 })
 next.addEventListener('click', ()=>{
   let translate = container.style.transform;
-  if(translate == 'translateX(0%)')
+  if(translate == 'translateX(0%)'){
     container.style.transform = `translateX(-33.333%)`;
-  else if (translate == 'translateX(-33.333%)')
+    next.style.color= "#d9eee6"
+    previous.style.color="#d9eee6"
+  }
+  else if (translate == 'translateX(-33.333%)') {
     container.style.transform = `translateX(-66.666%)`;
+    next.style.color= "transparent"
+  }
   else if(translate == 'translateX(-66.666%)')
     return false;
 
